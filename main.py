4990461f3077a63d647921ecacd9ed685b4b59cf
Midnight <23142346+Midnight145@users.noreply.github.com>
2023-11-12 11:50:04 -0600

    readme

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import copy
 import datetime
 import json
 import threading
@@ -14,24 +15,32 @@ with open("config.json", "r") as f:
 api_url = f'{config["api_url"]}:{config["api_port"]}'
 
 
+st.set_page_config(page_title="WatchfulWiggles", page_icon=":baby:", layout="wide")
+
+st.markdown("# WatchfulWiggles :baby:")
+st.markdown("Here you can view your baby monitors' live feeds as well as past events.")
+st.markdown("All of your registered devices are listed below. Click on a device to view its live feed,\
+ and expand the device to view its past events.")
+st.markdown("You can also view all events from all devices by clicking the button at the bottom of the page.")
+
 events = requests.get(f"{api_url}/api/events").json()
 sorted_events = {key: [] for key in set([i["device"] for i in events])}
 
 for event in events:
     sorted_events[event["device"]].append(event)
 
-devices = range(0, 6)
-
 for device in sorted(sorted_events.keys()):
-    st.markdown(f"## [Device {int(device)}]({api_url}/api/live/{int(device)})")
+    st.markdown(f"### [Device {int(device)}]({api_url}/api/live/{int(device)})")
     with st.expander("View Events"):
         rows = []
         for i in sorted_events[device]:
             rows.append([j for i, j in i.items()])
         for event in rows:
-            elapsed = int(event[2]) - int(event[1])
-            event[1] = datetime.datetime.fromtimestamp(int(event[1]))
-            event[2] = datetime.datetime.fromtimestamp(int(event[2]))
+            event[1] = int(float(event[1]))
+            event[2] = int(float(event[2]))
+            elapsed = event[2] - event[1]
+            event[1] = datetime.datetime.fromtimestamp(event[1])
+            event[2] = datetime.datetime.fromtimestamp(event[2])
             event.insert(3, strftime("%H:%M:%S", gmtime((event[2] - event[1]).total_seconds())))
             event[-1] = f"{api_url}/events/videos/{event[-1]}"
         for i in range(len(rows)):
@@ -63,4 +72,18 @@ for device in sorted(sorted_events.keys()):
             hide_index=True
         )
 
+st.markdown('### All events')
 
+to_display = copy.deepcopy(events)
+for event in to_display:
+    event["duration"] = int(float(event['movement_end'])) - int(float(event['movement_start']))
+    event['video_path'] = f"{api_url}/events/videos/{event['video_path']}"
+
+with st.expander('View All Events'):
+    st.dataframe(
+        data=to_display,
+        column_config={
+            "video_path": st.column_config.LinkColumn()
+        },
+        hide_index=True
+    )
