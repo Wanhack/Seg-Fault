0c2964822a0524ea2797c4701d4c18a136f00c78
Midnight <23142346+Midnight145@users.noreply.github.com>
2023-11-12 02:48:57 -0600

    update ui

--- main.py
+++ main.py
@@ -1,37 +1,58 @@
-import asyncio
-import time
-
-import uvicorn
+import datetime
+import json
 import threading
-from fastapi import FastAPI
+from time import strftime, gmtime
 
-import api
-from api import app
+import pandas as pd
 import streamlit as st
-import streamlit.components.v1 as components
+import uvicorn
+import api
 
+
+events = {}
 
 lock = threading.Lock()
 
+with open("config.json", "r") as f:
+    config = json.load(f)
 
 if __name__ == "__main__":
+    events = api.get_events()
+    sorted_events = {key: [] for key in set([i[b"device"] for i in events])}
 
-    iframe_src = "https://youtube.com"
-    components.iframe(iframe_src)
-    st.write("""
-        # My first app
-        Hello *world!*
-    """)
+    for event in events:
+        sorted_events[event[b"device"]].append(event)
+    with open("log.txt", "w") as f:
+        f.write(str(sorted_events))
 
-    for key, value in api.past_second.items():
-        st.image(value)
+    devices = range(0, 6)
 
-    threading.Thread(target=lambda: uvicorn.run("api:app", host="0.0.0.0", port=8000)).start()
+    for device in sorted_events.keys():
+        st.header(f"Device {int(device)}")
 
+        rows = []
+        for i in sorted_events[device]:
+            rows.append([j.decode("utf-8") for i, j in i.items()])
+        for event in rows:
+            event[1] = datetime.datetime.fromtimestamp(int(event[1]))
+            event[2] = datetime.datetime.fromtimestamp(int(event[2]))
+            event.insert(3, strftime("%H:%M:%S", gmtime((event[2] - event[1]).total_seconds())))
+            event[-1] = f"http://localhost:8000/events/videos/{event[-1]}"
+        for i in range(len(rows)):
+            del rows[i][0]
 
-async def display_video(id_: int):
-    # todo: move framerate to a constant
-    for key, value in api.past_second.values():
-        with lock.acquire():
-            st.image(value)
-            time.sleep(1/24)
+        st.subheader("Past Events")
+        data = pd.DataFrame(
+            columns=["Movement Start", "Movement End", "Time Elapsed", "Video Path"],
+            data=rows
+        )
+        st.dataframe(
+            data,
+            column_config={
+                "Video Path": st.column_config.LinkColumn()
+            },
+            hide_index=True
+        )
+
+    threading.Thread(target=lambda: uvicorn.run("api:app", host=config["api_host"], port=config["api_port"])).start()
+
