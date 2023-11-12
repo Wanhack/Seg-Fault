77516e034d05a13bae1a4e60c7d02198307d407f
Midnight <23142346+Midnight145@users.noreply.github.com>
2023-11-12 10:39:29 -0600

    add expanders

--- main.py
+++ main.py
@@ -24,43 +24,43 @@ devices = range(0, 6)
 
 for device in sorted(sorted_events.keys()):
     st.markdown(f"## [Device {int(device)}]({api_url}/api/live/{int(device)})")
+    with st.expander("View Events"):
+        rows = []
+        for i in sorted_events[device]:
+            rows.append([j for i, j in i.items()])
+        for event in rows:
+            elapsed = int(event[2]) - int(event[1])
+            event[1] = datetime.datetime.fromtimestamp(int(event[1]))
+            event[2] = datetime.datetime.fromtimestamp(int(event[2]))
+            event.insert(3, strftime("%H:%M:%S", gmtime((event[2] - event[1]).total_seconds())))
+            event[-1] = f"{api_url}/events/videos/{event[-1]}"
+        for i in range(len(rows)):
+            del rows[i][0]
+        st.markdown("### Notable Events")
+        notable_rows = [i for i in rows if i[1].timestamp() - i[0].timestamp() > 60]
 
-    rows = []
-    for i in sorted_events[device]:
-        rows.append([j for i, j in i.items()])
-    for event in rows:
-        elapsed = int(event[2]) - int(event[1])
-        event[1] = datetime.datetime.fromtimestamp(int(event[1]))
-        event[2] = datetime.datetime.fromtimestamp(int(event[2]))
-        event.insert(3, strftime("%H:%M:%S", gmtime((event[2] - event[1]).total_seconds())))
-        event[-1] = f"{api_url}/events/videos/{event[-1]}"
-    for i in range(len(rows)):
-        del rows[i][0]
-    st.markdown("### Notable Events")
-    notable_rows = [i for i in rows if i[1].timestamp() - i[0].timestamp() > 60]
+        st.dataframe(
+            pd.DataFrame(
+                columns=["Movement Start", "Movement End", "Time Elapsed", "Video Path"],
+                data=notable_rows
+            ),
+            column_config={
+                "Video Path": st.column_config.LinkColumn()
+            },
+            hide_index=True
+        )
 
-    st.dataframe(
-        pd.DataFrame(
-            columns=["Movement Start", "Movement End", "Time Elapsed", "Video Path"],
-            data=notable_rows
-        ),
-        column_config={
-            "Video Path": st.column_config.LinkColumn()
-        },
-        hide_index=True
-    )
+        st.markdown("#### All Events")
 
-    st.markdown("#### All Events")
-
-    st.dataframe(
-        data = pd.DataFrame(
-            columns=["Movement Start", "Movement End", "Time Elapsed", "Video Path"],
-            data=rows
-        ),
-        column_config={
-            "Video Path": st.column_config.LinkColumn()
-        },
-        hide_index=True
-    )
+        st.dataframe(
+            data = pd.DataFrame(
+                columns=["Movement Start", "Movement End", "Time Elapsed", "Video Path"],
+                data=rows
+            ),
+            column_config={
+                "Video Path": st.column_config.LinkColumn()
+            },
+            hide_index=True
+        )
 
 
