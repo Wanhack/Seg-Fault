4914d3f2d79fe37a82a14a0313f132ad4e992bbe
Midnight <23142346+Midnight145@users.noreply.github.com>
2023-11-12 04:10:49 -0600

    add testing client code

---
+++ client/client.py
@@ -0,0 +1,31 @@
+# import the opencv library
+import asyncio
+import io
+import cv2
+import aiohttp
+import json
+
+with open("config.json", "r") as f:
+    config = json.load(f)
+
+
+async def main():
+    cv2.destroyAllWindows()
+    async with aiohttp.ClientSession() as session:
+
+        # define a video capture object
+        vid = cv2.VideoCapture(0)
+        vid.set(cv2.CAP_PROP_FPS, 24)
+        try:
+            while True:
+                ret, frame = vid.read()
+                cv2.resize(frame, (600,600))
+                is_success, buffer = cv2.imencode(".jpg", frame)
+                io_buf = io.BytesIO(buffer)
+                async with session.post(f"{config["api_url"]}:{config["api_port"]}/api/frame/{config["device"]}", data={"file": io_buf}) as resp:
+                    pass
+        except:
+            vid.release()
+            cv2.destroyAllWindows()
+
+asyncio.run(main())
